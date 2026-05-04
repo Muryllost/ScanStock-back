844775b6c82796dfd2c0b3ff543d3bd9e688c09e
Muryllo S. Teixeira <162444926+Muryllost@users.noreply.github.com>
2026-05-04 09:34:12 -0300

    Update bd.js

--- src/bd.js
+++ src/bd.js
@@ -1,5 +1,5 @@
 import postgres from "postgres";
 
-const sql = postgres("postgresql://postgres:[User@muryllo]@db.wagwessyhwhdzrnyyuio.supabase.co:5432/postgres);
+const sql = postgres("postgresql://postgres:[User@muryllo]@db.wagwessyhwhdzrnyyuio.supabase.co:5432/postgres");
 
 export default sql;
